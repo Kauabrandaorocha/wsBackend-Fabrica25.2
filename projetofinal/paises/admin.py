81a0b34bf44c9071d0c7cbdc482df535b0bcde2f
kaua <kauavbrandaorocha@gmail.com>
2025-09-14 08:16:25 -0300

    :lipstick: feat | Adicionando admin

--- projetofinal/paises/admin.py
+++ projetofinal/paises/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pais, Idioma
 
 # Register your models here.
+class PaisAdmin(admin.ModelAdmin):
+    search_fields = ['nome_oficial']
+
+admin.site.register(Pais, PaisAdmin)
+admin.site.register(Idioma)
